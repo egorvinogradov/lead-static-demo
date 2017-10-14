4ce15e6c1c45ba8870561a35cae9d95467d17c20
Egor Vinogradov <egor@egor-Macbook-Pro.local>
2017-10-15 00:46:36 +0300

    Fix signup form

--- signup.php
+++ signup.php
@@ -2,20 +2,25 @@
 <?php include_file('includes/header', [signup_page => true]) ?>
 
 <div class="main-signin-page">
-  <div class="col-md-offset-8 col-md-4 main-controls">
-    <div><br>
-      <h3 class="text-center" style="font-size: 23px; font-weight: 500; text-transform: uppercase;">
-        <!-- react-text: 80 -->Connect your social media<!-- /react-text --><br>
-        <small>let artificial intelligence find the right match</small>
+  <div class="col-md-offset-8 col-md-4 main-controls text-center">
+    <div class="b-signup-7">
+      <h3 class="b-signup-9">
+        Connect your social media. <br>
+        Let artificial intelligence <span class="text-nowrap">find the</span> right match.
       </h3>
-      <hr>
-      <p class="text-center" style="font-weight: normal;">We use machine learning to detect users' personality traits
-        based on their background so that we can find mentors and mentees that are a perfect fit.</p>
-      <hr>
+
+      <hr class="m-b-30 m-t-30">
+      <p class="text-center gray m-b-30 b-signup-8">
+        We use machine learning to detect <span class="text-nowrap">users' personality</span> traits
+        based on their background so that we can find mentors and mentees that are a perfect fit.
+      </p>
       <form class="form-horizontal">
         <div class="form-group">
           <div class="col-sm-12">
-            <a href="/newsfeed" class="btn btn-info btn-block btn-lg">Connect Linkedin</a>
+            <a href="/newsfeed" class="b-signup-10 btn btn-info btn-block btn-lg">
+              <i class="fa fa-linkedin-square text-white"></i>
+              Connect LinkedIn
+            </a>
           </div>
         </div>
       </form>
